34422f583945b355985630cdd7f1f9c1ec30c244
Mirko Pica <picamirko02@gmail.com>
2022-06-25 22:44:41 +0200

    7 Reverse Integer

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
+
+        new_x = int(str(abs(x))[::-1])
+
+        if new_x > 2**31:
+            return 0
         
+        if x > 0:
+            return new_x
+        else:
+            return -new_x
 # @lc code=end
 
