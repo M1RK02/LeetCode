e56148ea4371953d60295ccd0a91d5c881d256d5
Mirko Pica <picamirko02@gmail.com>
2022-06-25 23:12:00 +0200

    14 Longest Common Prefix

---
+++ 14.longest-common-prefix.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=14 lang=python3
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+
+        prefix = strs[0]
+        for element in strs[1:]:
+            while not element.startswith(prefix):
+                prefix = prefix[:-1]
+                if len(prefix) == 0:
+                    return ""
+        
+        return prefix
+# @lc code=end
+
