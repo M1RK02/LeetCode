d131cf7fdf69874e442af546006c8eafe449edfc
Mirko Pica <picamirko02@gmail.com>
2022-06-26 20:07:08 +0200

    15. Three Sum

---
+++ 15.3-sum.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = set()
+        nums.sort()
+        
+        for i, val in enumerate(nums):
+            if i > 0 and val == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                threeSum = val + nums[left] + nums[right]
+                if threeSum > 0:
+                    right -= 1
+                elif threeSum < 0:
+                    left += 1
+                else:
+                    result.add((val,nums[left],nums[right]))
+                    left += 1
+                    right -= 1
+
+        return result
+# @lc code=end
+
